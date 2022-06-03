d00c3d7f13aa9d9bbfb93235d70cf1ff1bc3b833
LeandroRMachado <leandroskullofwar@outlook.com>
2022-06-03 13:38:00 -0300

    comunication between components

---
+++ src/components/WidgetForm/index.tsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+import { CloseButton } from "../CloseButton";
+
+import bugImageURL from '../../assets/bug.svg';
+import ideaImageURL from '../../assets/idea.svg';
+import thoughtImageURL from '../../assets/thought.svg';
+import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
+
+
+export const feedbackTypes = {
+  BUG: {
+    title: 'Problema',
+    image: {
+      source: bugImageURL,
+      alt: 'Imagem de uma minhoca'
+    }
+  },
+  IDEA: {
+    title: 'Ideia',
+    image: {
+      source: ideaImageURL,
+      alt: 'Imagem de uma lâmpada'
+    }
+  },
+  OTHER: {
+    title: 'Outro',
+    image: {
+      source: thoughtImageURL,
+      alt: 'Imagem de um balão de pensamento'
+    }
+  }
+};
+
+export type FeedbackType = keyof typeof feedbackTypes;
+
+export function WidgetForm() {
+  const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
+
+  return (
+    <div className="bg-white p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-3rem)] md:w-auto">
+      <header>
+        <span className="text-xl leading-6 text-zinc-800">Deixe seu feedback</span>
+        <CloseButton />
+      </header>
+
+      {!feedbackType ? (
+       <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType}/>
+      ) : (
+        <p>Hello World</p>
+      )}
+      
+
+      <footer className="text-xs text-zinc-500">
+        Feito com ♥ pelo <a href="https://github.com/LeandroRMachado" target="_blank" className="underline underline-offset-2">LeandroRMachado</a>
+      </footer>
+    </div>
+  );
+}
