a6a13c62296719a6dcbaf457ae6ee872400fd9c4
LeandroRMachado <leandroskullofwar@outlook.com>
2022-06-05 03:06:33 -0300

    finished button send feedback

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -33,8 +33,16 @@ export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested }
           className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none scrollbar-thumb-zinc-300 scrollbar-track-transparent scrollbar-thin "
           placeholder="Conte com detalhes o que está acontecendo..."
         />
-      </form>
 
+        <footer className="flex gap-2 mt-2">
+          <button
+            type="submit"
+            className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm text-white hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-brand-500 transition-colors"
+          >
+            Enviar feedback
+          </button>
+        </footer>
+      </form>
     </>
   )
 }
